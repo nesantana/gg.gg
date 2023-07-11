e2a804d4c64ccac32127c19e2b1dd4acbbf5988b
Mateus Santana <nesantana15@gmail.com>
2023-07-11 19:55:53 -0400

    gestor de jogos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+export default function App() {
+  const [nomeNovoJogo, setNomeNovoJogo] = useState('')
+  const [jogos, setJogos] = useState([])
+
+  function clicouParaAdicionarJogo() {
+    const novoJogos = [
+      ...jogos,
+      nomeNovoJogo
+    ]
+
+    setJogos(novoJogos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="bg-black h-screen flex justify-center">
+      <div className="w-8/12 text-white">
+        <h1 className="text-xl font-bold text-white text-center p-3">
+          gg.gg
+        </h1>
+        <input
+          placeholder="digite o nome do jogo"
+          className="w-full bg-transparent text-white border border-white px-3 rounded-md"
+          value={nomeNovoJogo}
+          onChange={(evento) => setNomeNovoJogo(evento.target.value)}
+        />
+        <div
+          className="text-sm mt-3 text-right cursor-pointer hover:underline"
+          onClick={() => clicouParaAdicionarJogo()}
         >
-          Learn React
-        </a>
-      </header>
+          adicionar jogo +
+        </div>
+        {
+          jogos.map(jogo => (
+            <div key={jogo} className="text-black p-5 bg-white rounded-md mt-3">
+              {jogo}
+            </div>
+          ))
+        }
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
